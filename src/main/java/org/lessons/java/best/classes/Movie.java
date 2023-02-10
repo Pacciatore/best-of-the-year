bda16cbfba1ff1dca3e67985d80f772738d76892
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-10 16:44:35 +0100

    creazione package per classi con classi Movie e Song vuote

---
+++ src/main/java/org/lessons/java/best/classes/Movie.java
@@ -0,0 +1,9 @@
+package org.lessons.java.best.classes;
+
+public class Movie {
+
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/org/lessons/java/best/classes/Song.java
@@ -0,0 +1,9 @@
+package org.lessons.java.best.classes;
+
+public class Song {
+
+	public Song() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
