811b3b103b60eea8e31997524054af74d0c6cd87
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-10 17:00:04 +0100

    aggiunto metodo getBestSongs()

--- src/main/java/org/lessons/java/best/controller/MainController.java
+++ src/main/java/org/lessons/java/best/controller/MainController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.lessons.java.best.classes.Movie;
+import org.lessons.java.best.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +34,20 @@ public class MainController {
 		movieList.add(new Movie(6, "Star Wars Episodio VI: Il ritorno dello Jedi"));
 
 		return movieList;
+	}
+
+	public List<Song> getBestSongs() {
+
+		List<Song> songList = new ArrayList<>();
+
+		songList.add(new Song(1, "Bohemian Rhapsody"));
+		songList.add(new Song(2, "Smells Like Teen Spirit"));
+		songList.add(new Song(3, "Baby Shark"));
+		songList.add(new Song(4, "Bollicine"));
+		songList.add(new Song(5, "Song2"));
+		songList.add(new Song(6, "Enter Sandman"));
+
+		return songList;
 	}
 
 }
