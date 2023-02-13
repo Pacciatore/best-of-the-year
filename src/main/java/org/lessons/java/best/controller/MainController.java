22b52ae2ef7172bfaa7a1b24ba0a395874117547
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2023-02-13 15:48:13 +0100

    Creazione di MovieController

--- src/main/java/org/lessons/java/best/controller/MainController.java
+++ src/main/java/org/lessons/java/best/controller/MainController.java
@@ -3,7 +3,6 @@ package org.lessons.java.best.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.lessons.java.best.classes.Movie;
 import org.lessons.java.best.classes.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,28 +18,6 @@ public class MainController {
 		model.addAttribute("mio_nome", "Tommaso");
 
 		return "index";
-	}
-
-	@GetMapping("/movies")
-	public String movies(Model model) {
-		model.addAttribute("mio_nome", "Tommaso");
-		model.addAttribute("movies", getBestMovies());
-
-		return "movies";
-	}
-
-	public List<Movie> getBestMovies() {
-
-		List<Movie> movieList = new ArrayList<>();
-
-		movieList.add(new Movie(1, "Star Wars Episodio I: La minaccia fantasma"));
-		movieList.add(new Movie(2, "Star Wars Episodio II: L'attacco dei cloni"));
-		movieList.add(new Movie(3, "Star Wars Episodio III: La vendetta dei Sith"));
-		movieList.add(new Movie(4, "Star Wars Episodio IV: Una nuova speranza"));
-		movieList.add(new Movie(5, "Star Wars Episodio V: L'impero colpisce ancora"));
-		movieList.add(new Movie(6, "Star Wars Episodio VI: Il ritorno dello Jedi"));
-
-		return movieList;
 	}
 
 	@GetMapping("/songs")
---
+++ src/main/java/org/lessons/java/best/controller/MovieController.java
@@ -0,0 +1,38 @@
+package org.lessons.java.best.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lessons.java.best.classes.Movie;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/movies")
+public class MovieController {
+	@GetMapping()
+	public String movies(Model model) {
+		model.addAttribute("mio_nome", "Tommaso");
+		model.addAttribute("movies", getBestMovies());
+
+		return "movies";
+
+	}
+
+	public List<Movie> getBestMovies() {
+
+		List<Movie> movieList = new ArrayList<>();
+
+		movieList.add(new Movie(1, "Star Wars Episodio I: La minaccia fantasma"));
+		movieList.add(new Movie(2, "Star Wars Episodio II: L'attacco dei cloni"));
+		movieList.add(new Movie(3, "Star Wars Episodio III: La vendetta dei Sith"));
+		movieList.add(new Movie(4, "Star Wars Episodio IV: Una nuova speranza"));
+		movieList.add(new Movie(5, "Star Wars Episodio V: L'impero colpisce ancora"));
+		movieList.add(new Movie(6, "Star Wars Episodio VI: Il ritorno dello Jedi"));
+
+		return movieList;
+	}
+
+}
